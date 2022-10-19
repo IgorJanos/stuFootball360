80e15c85952c5b44dcc6792fff9ea536cf07dcd4
Igor Janos <ijanos84@gmail.com>
2022-10-19 15:42:35 +0200

    Basic docker

---
+++ docker/run.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Fail on error and unset variables.
+set -eu -o pipefail
+
+CWD=$(readlink -e "$(dirname "$0")")
+cd "${CWD}/.." || exit $?
+source ./docker/common.sh
+
+DEVICE=0,1
+
+docker run \
+    -it --rm \
+    --name "stufootball" \
+    --runtime=nvidia \
+    --gpus all \
+    --privileged \
+    --shm-size 8g \
+    -v "${CWD}/..":/workspace \
+    -e CUDA_VISIBLE_DEVICES="$DEVICE" \
+    ${IMAGE_TAG} \
+    "$@" || exit $?
