0c94773379b2f1ac06c464e1be6975be4b5b073b
Igor Janos <ijanos84@gmail.com>
2022-10-24 08:15:00 -0400

    Support for Densenet, Resnet, EfficientNet

--- src/backstage/model.py
+++ src/backstage/model.py
@@ -25,7 +25,35 @@ def createDensenet(backbone):
     model.classifier = nn.Sequential()
     return model, numFeatures
 
+def createResnet(backbone):
+    if (backbone == "resnet18"): model = torchvision.models.resnet18(pretrained=True)
+    elif (backbone == "resnet34"): model = torchvision.models.resnet34(pretrained=True)
+    elif (backbone == "resnet50"): model = torchvision.models.resnet50(pretrained=True)
+    elif (backbone == "resnet101"): model = torchvision.models.resnet101(pretrained=True)
+    elif (backbone == "resnet152"): model = torchvision.models.resnet152(pretrained=True)
+    else:
+        return None, 0
 
+    numFeatures = model.classifier.in_features
+    model.classifier = nn.Sequential()
+    return model, numFeatures
+
+def createEfficientnet(backbone):
+    if (backbone == "efficientnet_b0"): model = torchvision.models.efficientnet_b0(pretrained=True)
+    elif (backbone == "efficientnet_b1"): model = torchvision.models.efficientnet_b1(pretrained=True)
+    elif (backbone == "efficientnet_b2"): model = torchvision.models.efficientnet_b2(pretrained=True)
+    elif (backbone == "efficientnet_b3"): model = torchvision.models.efficientnet_b3(pretrained=True)
+    elif (backbone == "efficientnet_b4"): model = torchvision.models.efficientnet_b4(pretrained=True)
+    elif (backbone == "efficientnet_b5"): model = torchvision.models.efficientnet_b5(pretrained=True)
+    elif (backbone == "efficientnet_b6"): model = torchvision.models.efficientnet_b6(pretrained=True)
+    elif (backbone == "efficientnet_b7"): model = torchvision.models.efficientnet_b7(pretrained=True)
+    else:
+        return None, 0
+
+    numFeatures = model.classifier[1].in_features
+    model.classifier = nn.Sequential()
+    return model, numFeatures
+    
 
 
 #------------------------------------------------------------------------------
@@ -39,6 +67,21 @@ SUPPORTED_MODELS = {
     "densenet161": createDensenet,
     "densenet169": createDensenet,
     "densenet201": createDensenet,
+
+    "resnet18": createResnet,
+    "resnet34": createResnet,
+    "resnet50": createResnet,
+    "resnet101": createResnet,
+    "resnet152": createResnet,
+
+    "efficientnet_b0": createEfficientnet,
+    "efficientnet_b1": createEfficientnet,
+    "efficientnet_b2": createEfficientnet,
+    "efficientnet_b3": createEfficientnet,
+    "efficientnet_b4": createEfficientnet,
+    "efficientnet_b5": createEfficientnet,
+    "efficientnet_b6": createEfficientnet,
+    "efficientnet_b7": createEfficientnet
 }
 
 
