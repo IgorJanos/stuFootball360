c82211b77bc4384c96e9d7d9ad9901d0770d4b96
Igor Janos <ijanos84@gmail.com>
2022-10-29 10:09:15 -0400

    Grafy z efficient netu

--- src/train.py
+++ src/train.py
@@ -24,12 +24,12 @@ def train():
         #"seta-densenet161.yaml",
         #"setb-densenet161.yaml",
         #"setc-densenet161.yaml",
-        #"seta-resnet152.yaml",
-        #"setb-resnet152.yaml",
-        #"setc-resnet152.yaml",
+        "seta-resnet152.yaml",
+        "setb-resnet152.yaml",
+        "setc-resnet152.yaml"
         #"seta-efficientnetb5.yaml",
         #"setb-efficientnetb5.yaml",
-        "setc-efficientnetb5.yaml"
+        #"setc-efficientnetb5.yaml"
     ]
     for c in configs:
         trainSingleRun(os.path.join(BASE_CONFIG_PATH, c))
